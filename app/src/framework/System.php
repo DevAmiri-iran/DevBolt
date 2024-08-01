042101fe052f0d8fcd4b1594cde055aaf4d1b0c1
DevAmiri <101191981+DevAmiri-iran@users.noreply.github.com>
2024-08-01 13:52:11 +0330

    Update System.php

--- app/src/framework/System.php
+++ app/src/framework/System.php
@@ -7,14 +7,13 @@ use App\System\Debugger;
 use App\System\ENV;
 use App\System\FileMaker;
 use App\System\Session;
-use App\System\Config;
 use App\System\Translator;
 use App\System\View;
 use App\System\Router;
 
 class System
 {
-    use ENV, FileMaker, Session, Database, Debugger, Config, Translator, View, Router;
+    use ENV, FileMaker, Session, Database, Debugger, Translator, View, Router;
 
     /**
      * Initialize the system.
@@ -25,7 +24,6 @@ class System
         if (self::$env_exit)
         {
             self::register_debugger();
-            self::config_loader();
             self::Translator_loader();
             self::start_session();
             self::config_view();
